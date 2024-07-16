35f899fdc7d486bca0a51d0e7ba1f327eef46516
Fede Barcelona <fede_rico_94@hotmail.com>
2024-07-16 10:35:30 +0200

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        sysdig-cli-scanner = pkgs.callPackage ./sysdig-cli-scanner.nix {};
+      in {
+        packages = {
+          inherit sysdig-cli-scanner;
+          default = sysdig-cli-scanner;
+        };
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              # Add here dependencies for the project.
+            ];
+          };
+
+        formatter = pkgs.alejandra;
+      }
+    );
+}
