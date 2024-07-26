766e8cc121f52b31e6864ba881878d4a53b53a3c
Fede Barcelona <fede_rico_94@hotmail.com>
2024-07-26 12:26:27 +0200

    fix: write database in the tmp dir

--- sysdig-cli-scanner.nix
+++ sysdig-cli-scanner.nix
@@ -1,6 +1,7 @@
 {
   stdenv,
   fetchurl,
+  makeWrapper,
 }: let
   versionMetadata = builtins.fromTOML (builtins.readFile ./sysdig-cli-scanner.versions.toml);
   fetchForSystem = versionMetadata.${stdenv.system} or (throw "unsupported system ${stdenv.system}");
@@ -13,9 +14,14 @@ in
       inherit (fetchForSystem) url hash;
     };
 
+    nativeBuildInputs = [makeWrapper];
+
     phases = ["installPhase"];
 
     installPhase = ''
       install -D $src $out/bin/sysdig-cli-scanner
+
+      wrapProgram $out/bin/sysdig-cli-scanner \
+        --add-flags --dbpath="/tmp/sysdig-cli-scanner/"
     '';
   }
